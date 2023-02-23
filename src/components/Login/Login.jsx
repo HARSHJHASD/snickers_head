6a5ed2a6eb1a5a8cf9d3ac85f67d44197c84a83d
Harsh Jha <harshjha19101997@gmail.com>
2023-02-22 21:45:33 -0800

    regex to login added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -8,24 +8,27 @@ function Login() {
   const [password, setPassword] = useState("");
   //state variable for storing registering email
   const [registerEmail, setRegisterEmail] = useState("");
-  //state variable for storing name used during registration 
+  //state variable for storing name used during registration
   const [registerName, setRegisterName] = useState("");
   //state variable for storing registration password
   const [registerPassword, setRegisterPassword] = useState("");
   //this is a token used for performing conditional rendering for login and signup page
   const [token, setToken] = useState(true);
-
+  //this is form logging out error based on input using regex
+  const [errorEmail, setErrorEmail] = useState("");
+  const [errorPassword, setErrorPassword] = useState("");
+  const [nameError,setNameError] = useState("");
+  // const [errorMessage, setErrorMessage] = useState("");
   // Define functions to update(using state) email and password incase of login
   function handleEmailChange(event) {
     event.preventDefault();
     setEmail(event.target.value);
   }
+  // /we are handling password change ,as user is entering password
   function handlePasswordChange(event) {
     event.preventDefault();
     setPassword(event.target.value);
   }
-
-
   //define and handle email,name ,password incase of registration ..(using usestate)
   function handleRegisterName(event) {
     event.preventDefault();
@@ -39,27 +42,77 @@ function Login() {
     event.preventDefault();
     setRegisterPassword(event.target.value);
   }
-
   // Define function to handle form submission(incase of login)
   function handleSubmitLogin(event) {
     // Prevent default form submission behavior
     event.preventDefault();
+    if (!email) {
+      setErrorEmail("Email is required");
+      return;
+    }
+    if (!password) {
+      setErrorPassword("Password is required");
+      return;
+    }
+    const emailRegex = /^\S+@\S+\.\S+$/;
+    if (!emailRegex.test(email)) {
+      setErrorEmail("Email is invalid");
+      return;
+    }
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+    if(!passwordRegex.test(password)){
+      setErrorPassword("Password must be of 8 digit,including 1digit,1 lowercase,1 uppercase letter")
+    }
+    if (password.length < 8) {
+      setErrorPassword("Password must be at least 8 characters long Atleast.");
+      return;
+    }
+
+    // //performing login using the axios...
+    // axios.post("http://localhost:3000/login", { email, password })
+    // .then((response) => {
+    //   console.log(response.data);
+    // })
+    // .catch((error) => {
+    //   console.log(error.response.data.message);
+    //   setErrorMessage(error.response.data.message);
+    // });
     // Log email and password values to console
     console.log(`Login Email: ${email}, LoginPassword: ${password}`);
   }
 
-
-  //define a fucntion fot handling for submission incase of registration page..
+  //define a function fot handling for submission incase of registration page..
   function handleSubmitRegister(event) {
     event.preventDefault();
+    const nameRegex = /^[A-Za-z\s]+$/i;
+    if(!nameRegex.test(registerName))
+    {
+      setNameError("Name should be alphabets only")
+    }
+    const emailRegex = /^\S+@\S+\.\S+$/;
+    if (!emailRegex.test(registerEmail)) {
+      setErrorEmail("Email is invalid");
+      return;
+    }
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+    if(!passwordRegex.test(registerPassword)){
+      setErrorPassword("Password must be of 8 digit,including 1digit,1 lowercase,1 uppercase letter")
+    }
+    if (password.length < 8) {
+      setErrorPassword("Password must be at least 8 characters long Atleast.");
+      return;
+    }
     // Log email and password values to console
     console.log(
       ` Register Name: ${registerName} ,Register Email: ${registerEmail},Register Password: ${registerPassword}`
     );
   }
-//this is for changing value of token ,for rendering 
+  //this is for changing value of token ,for rendering
   function handleRegisterButtonClick(event) {
     event.preventDefault();
+    setErrorPassword("")
+    setErrorEmail("")
+    setNameError("")
     setToken((prevState) => !prevState);
   }
 
@@ -86,6 +139,10 @@ function Login() {
               value={email}
               onChange={handleEmailChange}
             />
+            <div className="warning">
+            {errorEmail}
+            </div>
+            
           </div>
           {/* Password input field */}
           <div className="form-group">
@@ -101,19 +158,20 @@ function Login() {
               value={password}
               onChange={handlePasswordChange}
             />
+             <div className="warning">
+             {errorPassword}
+            </div>
+           
           </div>
           {/* Submit button */}
           <div>
-          <button id="loginButton" class="btn btn-block btn-lg btn-danger">
-            <span class="glyphicon glyphicon-arrow-right"></span> Login
-          </button>
-          <a id="register" onClick={handleRegisterButtonClick} href="">
-          Register.
-          </a>
+            <button id="loginButton" class="btn btn-block btn-lg btn-danger">
+              <span class="glyphicon glyphicon-arrow-right"></span> Login
+            </button>
+            <a id="register" onClick={handleRegisterButtonClick} href="">
+              Register
+            </a>
           </div>
-          
-
-
         </form>
       </div>
     );
@@ -142,6 +200,9 @@ function Login() {
               value={registerName}
               onChange={handleRegisterName}
             />
+               <div className="warning">
+             {nameError}
+            </div>
           </div>
 
           <div className="form-group">
@@ -157,6 +218,9 @@ function Login() {
               value={registerEmail}
               onChange={handleRegisterEmail}
             />
+               <div className="warning">
+             {errorEmail}
+            </div>
           </div>
 
           {/* Password input field */}
@@ -173,17 +237,19 @@ function Login() {
               value={registerPassword}
               onChange={handleRegisterPassword}
             />
+                   <div className="warning">
+             {errorPassword}
+            </div>
           </div>
           {/* Submit button */}
-          <div className="container">
-          <button id="loginButton" class="btn btn-block btn-lg btn-danger">
-            <span class="glyphicon glyphicon-arrow-right"></span> Register
-          </button>
-          <a id="register" onClick={handleRegisterButtonClick} href="">
-            Login Here
-          </a>
+          <div >
+            <button id="loginButton" class="btn btn-block btn-lg btn-danger">
+              <span class="glyphicon glyphicon-arrow-right"></span> Register
+            </button>
+            <a id="register" onClick={handleRegisterButtonClick} href="">
+              Login Here
+            </a>
           </div>
-         
         </form>
       </div>
     );
